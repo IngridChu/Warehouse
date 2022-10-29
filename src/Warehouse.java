ebad1596a3b2b7b9770d9dda49d09e6a7a3cb445
Ying Chen Chu <78563674+y1chu@users.noreply.github.com>
2022-10-29 01:20:58 -0700

    Fixed

--- src/Warehouse.java
+++ src/Warehouse.java
@@ -2,194 +2,131 @@ import java.util.*;
 
 public class Warehouse {
 
-    private static ArrayList<BetterItem> deliver = new ArrayList<>();
-    private static ArrayList<BetterItem> request = new ArrayList<>();
-    private static ArrayList<BetterItem> needToDeliver = new ArrayList<>();
-
-    private static ArrayList<Order> initial = new ArrayList<>();
+    // deliver arraylist
+    static ArrayList<String> deliverRaw = new ArrayList<String>();
+    static ArrayList<Item> deliver = new ArrayList<Item>();
+    // request arraylist
+    static ArrayList<String> requestRaw = new ArrayList<String>();
+    static ArrayList<Item> request = new ArrayList<Item>();
 
     public static void main(String[] args) {
-        // Day 0
+        Order firstOrder = SupplyAndDemand.initialStock();
         SupplyAndDemand.generateOrders();
-        initial.add(SupplyAndDemand.initialStock());
-        convertInitial(initial);
-        for (int i = 0; i <= 30; i++) {
-            deliver = sanitizeWarehouse(deliver);
-            request = sanitizeWarehouse(request);
-            needToDeliver = sanitizeWarehouse(needToDeliver);
-            checkAndRemoveStock(deliver, request, needToDeliver);
-            needToDeliver = sanitizeWarehouse(needToDeliver);
-            System.out.println();
-            System.out.println("Day " +  i + " Status: ");
-            System.out.println("===== Currently in Stock: =====");
-            printStatus(deliver);
-            System.out.println("===== Delivered today: =====");
-            printStatus(request);
-            System.out.println("===== Need to deliver: =====");
-            printStatus(needToDeliver);
-            removeNull();
-            getOrder(i);
-        }
-//        System.out.println(deliver);
-//        System.out.println("===========================SEPERATION LINE===========================");
-//        System.out.println(request);
+        deliverRaw.addAll(firstOrder.getItems());
 
-    }
+        for (int i = 0; i < 30; i++) {
+            Order nextOrder = SupplyAndDemand.nextOrder(i);
+            if (nextOrder != null) {
+                System.out.println("\nDay " + i + ": ");
+                // if contains "Deliver" then add to delivery arraylist
+                if (nextOrder.getType().contains("Deliver")) {
+                    deliverRaw.addAll(nextOrder.getItems());
+                    // System.out.println(deliverRaw);
+                    parse(deliverRaw, deliver);
+                    System.out.println("Deliver: " + deliver);
 
-    public static void printStatus(ArrayList<BetterItem> list) {
-        for (BetterItem betterItem : list) {
-            System.out.println(betterItem.getItemCount() + " " + betterItem.getItemName());
-        }
-    }
-
-    public static void checkAndRemoveStock(ArrayList<BetterItem> deliver, ArrayList<BetterItem> request, ArrayList<BetterItem> needToDeliver) {
-
-        for (BetterItem item : request) {
-            int count = 0;
-            for (BetterItem betterItem : deliver) {
-                // requesting the same item
-                if (betterItem.getItemName().equals(item.getItemName())) {
-                    int stockCount = betterItem.getItemCount();
-                    int requestCount = item.getItemCount();
-                    // if not enough
-                    if (stockCount < requestCount) {
-                        needToDeliver.add(item);
-                    }
-                    // if enough
-                    else {
-                        count++;
-                        betterItem.setItemCount(stockCount - requestCount);
-                    }
                 }
-            }
-            if (count == deliver.size()) {
-                needToDeliver.add(item);
-            }
-        }
-    }
+                // if contains "Request" then add to request arraylist
+                if (nextOrder.getType().contains("Request")) {
+                    requestRaw.addAll(nextOrder.getItems());
+                    // System.out.println(requestRaw);
+                    parse(requestRaw, request);
+                    // request will check if deliver has enough items
+                    // if not, then request will add to queue
+                    // if yes, then request will remove from deliver
+                    check(deliver, request);
+                    System.out.println("Request: " + request);
 
-    public static ArrayList<BetterItem> sanitizeWarehouse(ArrayList<BetterItem> type) {
-        // System.out.println("TYPE IS: " + type);
-        ArrayList<BetterItem> toReturn = new ArrayList<>();
-        ArrayList<String> stringOfItems = new ArrayList<>();
-        for (BetterItem item : type) {
-            if (stringOfItems.size() == 0) {
-                stringOfItems.add(item.getItemName());
-
-            }
-            int count = 0;
-            for (String betterItem : stringOfItems) {
-                if (!betterItem.equals(item.getItemName())) {
-                    count++;
                 }
-            }
-            if (count == stringOfItems.size()) {
-                stringOfItems.add(item.getItemName());
-            }
-        }
-        int count[] = new int [stringOfItems.size()];
 
-        for(int j = 0; j < stringOfItems.size(); j++) {
-            for (BetterItem betterItem : type) {
-                if (stringOfItems.get(j).equals(betterItem.getItemName())) {
-                    count[j] = count[j] + betterItem.getItemCount();
-                }
             }
         }
-        for(int i = 0; i < stringOfItems.size(); i++) {
-            toReturn.add(new BetterItem(count[i], stringOfItems.get(i)));
-        }
-        // System.out.println("TO_RETURN IS" + toReturn);
-        return toReturn;
+        System.out.println("\nEnd of Month Results: ");
+        System.out.println("Deliver: " + deliver);
+        System.out.println("Request: " + request);
     }
 
-    public static void convertInitial(ArrayList<Order> initial) {
-        for (Order order : initial) {
-            changeFormAndAdd(order, deliver);
-        }
-    }
-
-    public static void getOrder(int day) {
-        while (SupplyAndDemand.nextOrder(day) != null) {
-            Order current = SupplyAndDemand.nextOrder(day);
-            if (current != null && current.getType().equals("Deliver")) {
-                changeFormAndAdd(current, deliver);
-            } else if (current != null && current.getType().equals("Request")) {
-                changeFormAndAdd(current, request);
-            }
-        }
-    }
-
-    private static void changeFormAndAdd(Order current, ArrayList<BetterItem> requestOrDeliver) {
-        String changeForm = current.toString();
-        String[] changeFormArray = changeForm.split("\n");
-        changeFormArray = Arrays.copyOfRange(changeFormArray, 1, changeFormArray.length);
-        int itemCount;
-        String itemCountInString;
-        String item;
-        for (String s : changeFormArray) {
-            itemCountInString = "";
-            for (int j = 0; j < s.length(); j++) {
-                if (Character.isDigit(s.charAt(j))) {
-                    itemCountInString += Character.toString(s.charAt(j));
-                }
-            }
-            item = "";
-            for (int j = 0; j < s.length(); j++) {
-                if (!Character.isDigit(s.charAt(j))) {
-                    item += Character.toString(s.charAt(j));
-                }
-            }
-            itemCount = Integer.parseInt(itemCountInString);
-            BetterItem addTo = new BetterItem(itemCount, item);
-            requestOrDeliver.add(addTo);
-        }
-    }
-
-    public static void removeNull() {
-        for (int i = 0; i < deliver.size(); i++) {
-            if (deliver.get(i) == null) {
-                deliver.remove(i);
-                i--;
-            }
-        }
+    // check if deliver has enough items to fulfill request
+    // if not then leave it in request
+    // if yes then remove from deliver
+    public static void check(ArrayList<Item> deliver, ArrayList<Item> request) {
+        // System.out.println(request.size());
+        // System.out.println(deliver.size());
         for (int i = 0; i < request.size(); i++) {
-            if (request.get(i) == null) {
-                request.remove(i);
-                i--;
+            // System.out.println("Checking " + request.get(i) + " in request");
+            for (Item item : deliver) {
+                // System.out.println("Checking " + deliver.get(j) + " in deliver");
+                if (request.get(i).getName().equals(item.getName())) {
+                    if (request.get(i).getQuantity() <= item.getQuantity()) {
+                        System.out.println("Delivered " + request.get(i).getQuantity() + " " + request.get(i).getName());
+                        item.setQuantity(item.getQuantity() - request.get(i).getQuantity());
+                        request.remove(i);
+                        i--;
+                    }
+                }
             }
         }
     }
 
+
+    // parse the arraylist into item then add it to the new item arraylist
+    public static void parse(ArrayList<String> raw, ArrayList<Item> items) {
+        for (String s : raw) {
+            String[] split = s.split(" ");
+            int amount = Integer.parseInt(split[0]);
+            String type = split[3];
+            // loop through the items arraylist to see if the item already exists
+            boolean exists = false;
+            for (Item item : items) {
+                if (item.getName().equals(type)) {
+                    item.addAmount(amount);
+                    exists = true;
+                }
+            }
+            // if the item doesn't exist, create a new item and add it to the arraylist
+            if (!exists) {
+                Item newItem = new Item(type, amount);
+                items.add(newItem);
+            }
+        }
+
+    }
+
+
 }
 
-class BetterItem {
+class Item {
+    String name;
+    int quantity;
 
-    private int itemCount;
-    private String itemName;
-
-    public BetterItem(int itemCount, String itemName) {
-        this.itemCount = itemCount;
-        this.itemName = itemName;
+    public Item(String name, int quantity) {
+        this.name = name;
+        this.quantity = quantity;
     }
 
-    public int getItemCount() {
-        return itemCount;
+    public String getName() {
+        return name;
     }
 
-    public String getItemName() {
-        return itemName;
+    public int getQuantity() {
+        return quantity;
     }
 
-    public void setItemCount(int itemCount) {
-        this.itemCount = itemCount;
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
     }
 
-    @Override
     public String toString() {
-        return "BetterItem{" +
-                "itemCount=" + itemCount +
-                ", itemName='" + itemName + '\'' +
-                '}';
+        return quantity + " " + name;
+    }
+
+    public void addAmount(int amount) {
+        quantity += amount;
+    }
+
+    public void removeAmount(int amount) {
+        quantity -= amount;
     }
 }
+
+
